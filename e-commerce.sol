691cfaa724c1308dd7c1067f5a8a15ecdb83185d
manan-m-shah <shahmananvyara01@gmail.com>
2022-02-22 20:42:15 +0530

    Complete Contract

--- e-commerce.sol
+++ e-commerce.sol
@@ -1,59 +1,84 @@
 //SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.12;
+
+pragma solidity >=0.5.0 <0.9.0;
 
 contract Ecommerce {
     struct Product {
         string title;
-        string description;
-        uint256 price;
+        string desc;
         address payable seller;
         uint256 productId;
+        uint256 price;
         address buyer;
         bool delivered;
     }
+    address payable manager;
+    bool destroyed = false;
 
-    uint256 counter = 0;
+    constructor() {
+        manager = payable(msg.sender);
+    }
+
+    uint256 counter = 1;
     Product[] public products;
 
-    function register(
+    modifier isNotDestroyed() {
+        require(!destroyed, "Contract does not exist");
+        _;
+    }
+
+    function registerProduct(
         string memory _title,
-        string memory _description,
+        string memory _desc,
         uint256 _price
-    ) public {
-        require(_price > 0);
-
-        Product memory newProduct;
-
-        newProduct.title = _title;
-        newProduct.description = _description;
-        newProduct.price = _price * 10**18;
-        newProduct.seller = payable(msg.sender);
-        newProduct.productId = counter;
-
-        products.push(newProduct);
+    ) public isNotDestroyed {
+        require(_price > 0, "Price should be greater than zero");
+        Product memory tempProduct;
+        tempProduct.title = _title;
+        tempProduct.desc = _desc;
+        tempProduct.price = _price * 10**18;
+        tempProduct.seller = payable(msg.sender);
+        tempProduct.productId = counter;
+        products.push(tempProduct);
+        counter++;
     }
 
-    function buy(uint256 _productId) public payable {
+    function buy(uint256 _productId) public payable isNotDestroyed {
         require(
-            products[_productId].price == msg.value,
-            "Incorrect price paid."
+            products[_productId - 1].price == msg.value,
+            "Pay the exact price"
         );
         require(
-            products[_productId].seller != msg.sender,
-            "You either purchase OR sell!"
+            products[_productId - 1].seller != msg.sender,
+            "Seller cannot buy"
         );
-
-        products[_productId].buyer = msg.sender;
+        products[_productId - 1].buyer = msg.sender;
     }
 
-    function delivered(uint256 _productId) public {
+    function delivery(uint256 _productId) public isNotDestroyed {
         require(
-            products[_productId].buyer == msg.sender,
-            "Only buyer can confirm delivery."
+            products[_productId - 1].buyer == msg.sender,
+            "Only buyer can confirm"
         );
+        products[_productId - 1].delivered = true;
+        products[_productId - 1].seller.transfer(
+            products[_productId - 1].price
+        );
+    }
 
-        products[_productId].delivered = true;
+    // function destroy() public{
+    //  require(manager==msg.sender,"Only manager can call this function");
+    //  selfdestruct(manager);
+    // //  Not the right way to destroy the contract
+    // }
 
-        products[_productId].seller.transfer(products[_productId].price);
+    function destroy() public isNotDestroyed {
+        require(manager == msg.sender, "only manager can call this");
+        manager.transfer(address(this).balance);
+        destroyed = true;
+    }
+
+    fallback() external payable {
+        payable(msg.sender).transfer(msg.value);
     }
 }
